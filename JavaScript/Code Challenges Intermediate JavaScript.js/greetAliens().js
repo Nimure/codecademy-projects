af08d2b5585519160423128909d84d60db4c6d47
Nimure <kechara19@gmail.com>
2022-12-19 22:27:57 -0500

    initial commit

---
+++ JavaScript/Code Challenges Intermediate JavaScript.js/greetAliens().js
@@ -0,0 +1,19 @@
+// Write your code here:
+
+const greetAliens = (string) => {
+    for (let i = 0; i < string.length; i++) {
+      console.log('Oh powerful ' + string[i] + ', we humans offer our unconditional surrender!');
+    }
+  };
+  
+  
+  
+  
+  
+  
+  
+  // When you're ready to test your code, uncomment the below and run:
+  
+  const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+  
+  greetAliens(aliens);
